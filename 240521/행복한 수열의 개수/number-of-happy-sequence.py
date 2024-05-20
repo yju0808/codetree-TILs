8fb9d56229050d6a62f33a6fcfa4bfe4d226804d
Kant Lim <yju0808@naver.com>
2024-05-21 02:56:38 +0900

    Title: 행복한 수열의 개수, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240521/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,43 @@
+n, m = map(int, input().split())
+
+board = [list(map(int, input().split())) for _ in range(n)]
+
+happy_count = 0
+
+for i in range(n):
+
+    before = None
+    continuous_count = 0
+
+    for num in board[i]:
+
+        if num != before:
+            continuous_count = 1
+        else:
+            continuous_count += 1
+
+        if continuous_count == m:
+            happy_count += 1
+            break
+
+        before = num
+
+    before = None
+    continuous_count = 0
+
+    for j in range(n):
+
+        if board[j][i] != before:
+            continuous_count = 1
+            
+        else:
+            continuous_count += 1
+
+        if continuous_count == m:
+            happy_count += 1
+            break
+
+        before = num
+
+        
+print(happy_count)

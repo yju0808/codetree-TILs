08f707034b3ba5d06f6c8157efaa1cce279b49be
Kant Lim <yju0808@naver.com>
2024-08-24 19:27:30 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240824/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240824/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -6,18 +6,20 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 def is_vaild_coord(y, x):
     return 0 <= y < n and 0 <= x < m
 
-def get_score(y, x , h, w, checked):
+def get_score(y, x , h, w, checked, is_two):
 
     result = 0
 
     for i in range(h):
         for j in range(w):
 
-            if not is_vaild_coord(y + i, x + j) or checked[y + i][x + j]:
+            if not is_vaild_coord(y + i, x + j) or (y + i, x + j) in checked:
                 return -float('inf')
 
             result += grid[y + i][x + j]
-            checked[y + i][x + j] = True
+
+            if not is_two:
+                checked.add((y + i, x + j))
 
 
     return result
@@ -31,16 +33,16 @@ for i in range(n):
             for w in range(1, m):
 
                 result = 0
-                checked = [[False for _ in range(m)] for _ in range(n)]
+                checked = set()
                 
-                result += get_score(i, j, h, w, checked)
+                result += get_score(i, j, h, w, checked, False)
 
                 for i2 in range(n):
                     for j2 in range(m):
                         for h2 in range(1, n):
                             for w2 in range(1, m):
 
-                                result += get_score(i2, j2, h2, w2, checked)
+                                result += get_score(i2, j2, h2, w2, checked, True)
                                 ans = max(result, ans)
 
                 

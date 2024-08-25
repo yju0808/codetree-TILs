518c9d4bea1e9f4d10e403c5760779f1b4d4dd87
Kant Lim <yju0808@naver.com>
2024-08-25 20:59:50 +0900

    Title: 1차원 폭발 게임, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240825/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,57 @@
+n, m = map(int, input().split())
+
+bombs_a = [int(input()) for _ in range(n)]
+
+bombs_b = []
+
+
+while True:
+
+    is_ended = True
+
+    
+    count = 0
+
+    temp = bombs_a[0]
+
+    while bombs_a:
+        
+        current = bombs_a.pop()
+
+        if current != temp:
+            
+            if count >= m:
+
+                while bombs_b and bombs_b[-1] == temp:
+                    bombs_b.pop()
+                is_ended = False
+                
+                count = 1
+
+            bombs_b.append(current)
+            temp = current
+                
+        else:
+            count += 1
+            bombs_b.append(current)
+
+
+    
+    if count >= m and bombs_b:
+        while bombs_b and bombs_b[-1] == temp:
+                bombs_b.pop()
+        is_ended = False
+
+    while bombs_b:
+        bombs_a.append(bombs_b.pop())
+
+    if not bombs_a:
+        is_ended = True
+
+    if is_ended:
+        break
+
+
+print(len(bombs_a))
+for bomb in bombs_a:
+    print(bomb)

63c8fefabedb66bf83ef78735b9968a1264540a4
Kant Lim <yju0808@naver.com>
2024-09-01 18:50:10 +0900

    Title: 벽이 있는 충돌 실험, Time: 5031ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240901/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240901/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -13,6 +13,9 @@ for _ in range(t):
 
     n, m = map(int, input().split())
 
+    beads = set()
+    temp_beads = set()
+
     grid = [[-1 for _ in range(n)] for _ in range(n)]
     temp_grid = [[-1 for _ in range(n)] for _ in range(n)]
 
@@ -26,35 +29,45 @@ for _ in range(t):
         x -= 1
 
         grid[y][x] = direction_mapper[d]
+        beads.add((y, x))
 
     for _ in range(n * n):
-        
-        for y in range(n):
-            for x in range(n):
 
-                if grid[y][x] >= 0:
+        for y, x in beads:
+            
+            d = grid[y][x]
+            ny = y + dy[d]
+            nx = x + dx[d]
+
+            if grid[y][x] >= 0:
                     
-                    d = grid[y][x]
+                d = grid[y][x]
 
-                    ny = y + dy[d]
-                    nx = x + dx[d]
+                ny = y + dy[d]
+                nx = x + dx[d]
 
-                    # 벽에 부딧히는 경우
-                    if not is_valid_coord(ny, nx, n):
+                # 벽에 부딧히는 경우
+                if not is_valid_coord(ny, nx, n):
 
-                        if temp_grid[y][x] >= 0:
-                            temp_grid[y][x] = -1
+                    if temp_grid[y][x] >= 0:
+                        temp_grid[y][x] = -1
+                        temp_beads.remove((y, x))
 
-                        else:
-                            temp_grid[y][x] = (d + 2) % 4
+                    else:
+                        temp_grid[y][x] = (d + 2) % 4
+                        temp_beads.add((y, x))
 
-                    # 구슬끼리 충돌하는 경우
-                    elif temp_grid[ny][nx] >= 0:
-                        temp_grid[ny][nx] = -1
+                # 구슬끼리 충돌하는 경우
+                elif temp_grid[ny][nx] >= 0:
+                    temp_grid[ny][nx] = -1
+                    temp_beads.remove((ny, nx))
 
-                    # 문제 없는 경우
-                    elif temp_grid[ny][nx] == -1:
-                        temp_grid[ny][nx] = d
+                # 문제 없는 경우
+                elif temp_grid[ny][nx] == -1:
+                    temp_grid[ny][nx] = d
+                    temp_beads.add((ny, nx))
+
+
 
 
         for i in range(n):
@@ -65,12 +78,8 @@ for _ in range(t):
             for j in range(n):
                 temp_grid[i][j] = -1
 
+        beads = temp_beads
+        temp_beads = set()
 
-    ans = 0
 
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] >= 0:
-                ans += 1
-    
-    print(ans)
+    print(len(beads))

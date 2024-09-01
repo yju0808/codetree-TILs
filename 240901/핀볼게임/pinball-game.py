f43a68d967b6df073d8870208f1f8fc2281c3d12
Kant Lim <yju0808@naver.com>
2024-09-01 17:36:21 +0900

    Title: 핀볼게임, Time: 5103ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240901/핀볼게임/pinball-game.py
@@ -0,0 +1,68 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+
+dy = [0, -1, 0, 1]
+dx = [1, 0, -1, 0]
+
+
+def is_valid_coord(y, x):
+    return 0 <= y < n and 0 <= x < n
+
+
+
+def simul(y, x, direction):
+
+    time = 1
+
+    while is_valid_coord(y, x):
+
+        if grid[y][x] == 1:
+            
+            if direction <= 1:
+                direction = 1
+
+            elif direction == 2:
+                direction = 3
+
+            elif direction == 3:
+                direction = 2
+
+
+        elif grid[y][x] == 2:
+            if direction == 0:
+                direction = 3
+
+            elif direction == 1:
+                direction = 2
+
+            elif direction == 2:
+                direction = 1
+
+            elif direction == 3:
+                direction = 0
+
+
+        y += dy[direction]
+        x += dx[direction]
+
+        time += 1
+
+
+    return time
+
+ans = 0
+
+
+for i in range(n):
+    ans = max(ans, simul(i, 0, 0))
+    ans = max(ans, simul(i, n - 1, 2))
+
+
+for j in range(n):
+    ans = max(ans, simul(0, j, 3))
+    ans = max(ans, simul(n - 1, j, 1))
+
+
+print(ans)

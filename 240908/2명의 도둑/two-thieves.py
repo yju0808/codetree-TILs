85aeab608bb35c639b3dbe7b240de7686ede3e25
Kant Lim <yju0808@naver.com>
2024-09-08 15:19:30 +0900

    Title: 2명의 도둑, Time: 696ms, Memory: 29MB, Status: Passed - CodeTree

--- 240908/2명의 도둑/two-thieves.py
+++ 240908/2명의 도둑/two-thieves.py
@@ -3,59 +3,112 @@ n, m, c = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 selected_point = []
 
+
+ans = 0
+
 def is_valid_coord(y, x):
     return 0 <= y < n and 0 <= x < n
 
 
-def simul(points):
+def cal_score(nums):
 
     result = 0
 
-    if is_overlaped(points):
+    if sum(nums) > c:
         return 0
 
-    for y, x in points:
+    for num in nums:
+        result += num * num
 
-        for i in range(x, x + m):
-            
+    return result
+
+
+
+def simul(p1, p2):
+
+    global ans
+
+    if is_overlaped(p1, p2):
+        return 0
+
+    y1, x1 = p1
+    y2, x2 = p2
+
+    nums1 = []
+    nums2 = []
+
+    for i in range(x1, x1 + m):
+
+        if i >= n:
+            break
+
+        nums1.append(grid[y1][i])
+
+    for i in range(x2, x2 + m):
+
+        if i >= n:
+            break
+
+        nums2.append(grid[y2][i])
+
+    result = 0
+
     
-    pass
+
+    w1 = select(nums1, 0, 0)
+    w2 = select(nums2, 0, 0)
+
+    result = w1 + w2
+
+    ans = max(ans, result)
 
 
 
-def is_overlaped(points):
 
-    for i in range(len(points)):
+def select(nums, k, max_weight):
 
-        for j in range(len(points)):
+    if k == len(nums):
+        score = cal_score(selected_point)
+        max_weight = max(score, max_weight)
+        return max_weight
 
-            if i == j:
-                continue
+    
+    selected_point.append(nums[k])
 
-            y1, x1 = points[i]
-            y2, x2 = points[j]
+    max_weight = max(select(nums, k + 1, max_weight), max_weight)
 
-            if y1 == y2:
+    selected_point.pop()
 
-                left = min(x1, x2)
-                right = max(x1, x2)
-                
-                if left + m - 1 >= right
-                    return True
+    max_weight = max(select(nums, k + 1, max_weight), max_weight)
+
+    return max_weight
+
+
+
+
+def is_overlaped(p1, p2):
+
+    y1, x1 = p1
+    y2, x2 = p2
+
+    if y1 != y2:
+        return False
+
+    right = max(x1, x2)
+    left = min(x1, x2)
+
+    if left + m - 1 >= right:
+        return True
+
     return False
+
 
 
 for i in range(n):
     for j in range(n):
         for k in range(n):
             for l in range(n):
-                pass
+                simul((i, j), (k, l))
 
 
-
-
-
-
-
-
-select(0, 0)
+print(ans)

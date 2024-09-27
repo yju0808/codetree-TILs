e56cf483ef5d3e60a88c6c71248ad5013feb4cb8
Kant Lim <yju0808@naver.com>
2024-09-27 19:03:18 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240927/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 240927/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -10,8 +10,8 @@ def is_valid_coord(y, x):
     return 0 <= y < n and 0 <= x < n
 
 
-dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
-dys = [0, 1, 1, 1, 0, -1, -1, -1]
+dys = [-1, -1, 0, 1, 1, 1, 0, -1]
+dxs = [0, 1, 1, 1, 0, -1, -1, -1]
 
 
 ans = 0
@@ -25,7 +25,7 @@ def solve(y, x, count):
     d = direction[y][x] - 1
 
     for i in range(n):
-        nx, ny = x + dxs[d] * i, y + dys[d] * i
+        ny, nx = y + dys[d] * i, x + dxs[d] * i
 
         if is_valid_coord(ny, nx) and grid[ny][nx] > grid[y][x]:
             solve(ny, nx, count + 1)
@@ -33,8 +33,8 @@ def solve(y, x, count):
 
 y, x = map(int, input().split())
 
-y -= 1
 x -= 1
+y -= 1
 
 solve(y, x, 0)
 print(ans)

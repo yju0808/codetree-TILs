2f1d55c98e625ac834b4f1c1f6954382df2899ff
Kant Lim <yju0808@naver.com>
2024-09-28 13:22:26 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 5076ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240928/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -0,0 +1,49 @@
+n = int(input())
+
+nums = ['4', '5', '6']
+
+
+def is_possible(sequence):
+    
+    for idx in range(len(sequence)):
+        
+        length = 1
+        while True:
+      
+            start1, end1 = idx, idx + length - 1
+            start2, end2 = end1 + 1, (end1 + 1) + length - 1
+  
+            if end2 >= len(sequence):
+                break
+      
+            if sequence[start1:end1 + 1] == sequence[start2:end2 + 1]:
+                return False
+            
+            length += 1
+
+    return True
+
+
+candi = []
+
+ans = ['9']
+
+
+def solve():
+
+    global ans
+
+    if len(candi) == n:
+        if is_possible(candi):
+            ans = min(ans, candi[:])
+        return
+
+
+    for i in range(3):
+        candi.append(nums[i])
+        solve()
+        candi.pop()
+
+
+solve()
+print("".join(ans))

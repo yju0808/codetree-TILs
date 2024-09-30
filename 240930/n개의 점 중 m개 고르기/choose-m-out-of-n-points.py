9affd5ac296546477c1c1b2bdfde57e542f21d1a
Kant Lim <yju0808@naver.com>
2024-09-30 19:27:41 +0900

    Title: n개의 점 중 m개 고르기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240930/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240930/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -10,7 +10,7 @@ def cal_dist(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
 
-    return math.sqrt(((x1 - x2) ** 2 + (y1 - y2) ** 2))
+    return math.sqrt(int((x1 - x2) ** 2 + (y1 - y2) ** 2))
 
 ans = float('inf')
 
@@ -26,7 +26,6 @@ def cal_ans(selected):
                 continue
 
             result = max(result, cal_dist(selected[i], selected[j]))
-
 
     return result
 

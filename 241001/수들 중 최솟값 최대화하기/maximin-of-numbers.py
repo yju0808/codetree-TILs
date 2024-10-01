54a400ba0964274bad905b4ff5a49d99370ee388
Kant Lim <yju0808@naver.com>
2024-10-01 15:27:55 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 1199ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 241001/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,47 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+
+nums = [i for i in range(n * n)]
+selected = []
+
+visited_j = [False for _ in range(n)]
+
+def cal_socre(selected):
+    result = float('inf')
+
+    for row, col in selected:
+        result = min(result, grid[row][col])
+
+    return result
+
+ans = -1
+
+
+def solve(last_i):
+
+    global ans
+
+    if len(selected) == n:
+        ans = max(ans, cal_socre(selected))
+        return
+
+    for i in range(last_i + 1, n):
+        for j in range(n):
+
+            if visited_j[j]:
+                continue
+            
+            selected.append((i, j))
+            visited_j[j] = True
+
+            solve(i)
+
+            selected.pop()
+            visited_j[j] = False
+
+
+
+solve(-1)
+print(ans)

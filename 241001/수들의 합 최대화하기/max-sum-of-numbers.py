356367835816f63683b0f1df4b9a10c63577fd93
Kant Lim <yju0808@naver.com>
2024-10-01 14:58:13 +0900

    Title: 수들의 합 최대화하기, Time: 1135ms, Memory: 28MB, Status: Passed - CodeTree

--- 241001/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 241001/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -5,7 +5,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 nums = [i for i in range(n * n)]
 selected = []
-visited_i = [False for _ in range(n)]
+
 visited_j = [False for _ in range(n)]
 
 def cal_socre(selected):
@@ -19,7 +19,7 @@ def cal_socre(selected):
 ans = -1
 
 
-def solve(last_i, last_j):
+def solve(last_i):
 
     global ans
 
@@ -27,23 +27,21 @@ def solve(last_i, last_j):
         ans = max(ans, cal_socre(selected))
         return
 
-    for i in range(n):
+    for i in range(last_i + 1, n):
         for j in range(n):
 
-            if visited_i[i] or visited_j[j]:
+            if visited_j[j]:
                 continue
             
             selected.append((i, j))
-            visited_i[i] = True
             visited_j[j] = True
 
-            solve(i, j)
+            solve(i)
 
             selected.pop()
-            visited_i[i] = False
             visited_j[j] = False
 
 
 
-solve(-1, -1)
+solve(-1)
 print(ans)

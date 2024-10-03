44a3e9b844538ea1b496715e468ec89203963a95
Kant Lim <yju0808@naver.com>
2024-10-03 14:33:35 +0900

    Title: 빙하, Time: 236ms, Memory: 28MB, Status: Passed - CodeTree

--- 241003/빙하/glacier.py
+++ 241003/빙하/glacier.py
@@ -5,7 +5,7 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 
-dq = deque()
+
 
 dy = [0, -1, 0, 1]
 dx = [1, 0, -1, 0]
@@ -18,36 +18,51 @@ def is_valid_coord(y, x):
 
 
 
-dq.append((0, 0, 0))
+
 
 ans_time = 0
 time_for_size = dict()
 
+
+dq = deque()
+dq.append((0, 0, 0))
+temp_dq = deque()
+
+
 while dq:
 
-    y, x, time = dq.popleft()
+    for y, x, time in dq:
+        grid[y][x] = 0
 
-    ans_time = max(time, ans_time)
 
-    if grid[y][x] == 1:
         if time in time_for_size:
             time_for_size[time] += 1
         else:
             time_for_size[time] = 1
 
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
 
-        if is_valid_coord(ny, nx) and not visited[ny][nx]:
 
-            if grid[ny][nx] == 1:
-                dq.append((ny, nx, time + 1))
+    while dq:
+        temp_dq.append(dq.popleft())
 
-            else:
-                dq.append((ny, nx, time))
+    while temp_dq:
 
-            visited[ny][nx] = True
+        y, x, time = temp_dq.popleft()
+        ans_time = max(time, ans_time)
+            
 
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if is_valid_coord(ny, nx) and not visited[ny][nx] and grid[y][x] != 1:
+
+                if grid[ny][nx] == 1:
+                    dq.append((ny, nx, time + 1))
+
+                else:
+                    temp_dq.append((ny, nx, time))
+
+                visited[ny][nx] = True
 
 print(ans_time, time_for_size[ans_time])

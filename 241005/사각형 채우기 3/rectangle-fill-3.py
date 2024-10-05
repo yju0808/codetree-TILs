f5b6d4952d788988e4f95c3902511ea721cfc78c
Kant Lim <yju0808@naver.com>
2024-10-05 14:43:41 +0900

    Title: 사각형 채우기 3, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241005/사각형 채우기 3/rectangle-fill-3.py
+++ 241005/사각형 채우기 3/rectangle-fill-3.py
@@ -15,7 +15,7 @@ def get_answer(n):
     dp[n] = (get_answer(n - 1) * 2 + get_answer(n - 2) * 3 ) % 1000000000007
 
     for i in range(n - 3, -1, -1):
-        dp[n] += get_answer(i) * 2
+        dp[n] += (get_answer(i) * 2) % 1000000000007
 
     return dp[n]
 

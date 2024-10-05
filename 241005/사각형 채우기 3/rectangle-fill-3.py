a91db628af6a659b0819862294a5f58dec255bf7
Kant Lim <yju0808@naver.com>
2024-10-05 14:43:25 +0900

    Title: 사각형 채우기 3, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241005/사각형 채우기 3/rectangle-fill-3.py
+++ 241005/사각형 채우기 3/rectangle-fill-3.py
@@ -2,6 +2,7 @@ n = int(input())
 
 dp = [0 for _ in range(n + 3)]
 
+dp[0] = 1
 dp[1] = 2
 dp[2] = 7
 
@@ -11,7 +12,10 @@ def get_answer(n):
         return dp[n]
 
 
-    dp[n] = (get_answer(n - 1) * 2 + get_answer(n - 2) * 3 + 2) % 1000000000007
+    dp[n] = (get_answer(n - 1) * 2 + get_answer(n - 2) * 3 ) % 1000000000007
+
+    for i in range(n - 3, -1, -1):
+        dp[n] += get_answer(i) * 2
 
     return dp[n]
 

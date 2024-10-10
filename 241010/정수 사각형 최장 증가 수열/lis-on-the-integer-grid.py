7e720ece57950209c5cd348af0163f31c4cfb4d1
Kant Lim <yju0808@naver.com>
2024-10-10 20:04:52 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1108ms, Memory: 56MB, Status: Passed - CodeTree

--- 241010/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 241010/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -5,26 +5,36 @@ dx = [1, 0, -1, 0]
 
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0 for _ in range(n)] for _ in range(n)]
+dp = [[1 for _ in range(n)] for _ in range(n)]
 
+
+nums = []
+
+for i in range(n):
+    for j in range(n):
+        nums.append((grid[i][j], i, j))
+
+nums.sort()
 
 def is_valid_coord(y, x):
     return 0 <= y < n and 0 <= x < n
 
-def can_gone(y1, x1, y2, x2):
-    if grid[y1][x1] <= grid[y2][x2]:
-        return True
 
-    return False
-
-def get_answer(y, x):
-    
-    if dp[i][j]:
-        return dp[i][j]
+for _, y, x in nums:
 
     for i in range(4):
-
         ny = y + dy[i]
         nx = x + dx[i]
 
-        if is_valid_coord(ny, nx) and can_gone(y, x, ny, nx):
+        if is_valid_coord(ny, nx) and grid[ny][nx] > grid[y][x]:
+            dp[ny][nx] = max(dp[ny][nx], dp[y][x] + 1)
+
+
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        ans = max(ans, dp[i][j])
+
+print(ans)

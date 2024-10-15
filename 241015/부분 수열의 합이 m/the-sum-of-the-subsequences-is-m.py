6b9b757bf4b8f10a292e0c65ec89da8e48281ac9
Kant Lim <yju0808@naver.com>
2024-10-15 21:19:06 +0900

    Title: 부분 수열의 합이 m, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241015/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 241015/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -1,37 +1,32 @@
-MAX_ANS = 101
+import sys
 
-# 변수 선언 및 입력:
+sys.setrecursionlimit(10**5)
+
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
 
-# 메모이제이션을 위한 dp 배열 생성
-# dp[i][rem] : i번째 원소부터 끝까지 사용하여 남은 금액 rem을 만들기 위한 최소 수열의 길이
-dp = [[-1] * (m + 1) for _ in range(n + 1)]
+nums = list(map(int, input().split()))
 
-def dfs(index, rem):
-    # 남은 금액을 정확히 0으로 만들었을 때
-    if rem == 0:
+dp = [[-1 for _ in range(m + 1)] for _ in range(n)]
+
+def get_answer(index, k):
+
+    if k == 0:
         return 0
-    # 인덱스를 벗어나거나 남은 금액이 음수가 된 경우
-    if index == n or rem < 0:
-        return MAX_ANS
-    # 이미 계산한 값이 있으면 반환
-    if dp[index][rem] != -1:
-        return dp[index][rem]
-    
-    # 현재 원소를 선택하지 않는 경우
-    res = dfs(index + 1, rem)
-    # 현재 원소를 선택하는 경우 (각 원소를 최대 한 번씩만 선택)
-    if rem >= arr[index]:
-        res = min(res, dfs(index + 1, rem - arr[index]) + 1)
-    
-    dp[index][rem] = res
-    return res
 
-min_len = dfs(0, m)
+    if index >= len(nums) or k < 0:
+        return float('inf')
 
-# 합 m을 만드는 것이 불가능 할 시, -1을 출력합니다.
-if min_len >= MAX_ANS:
-    min_len = -1
+    if dp[index][k] != -1:
+        return dp[index][k]
 
-print(min_len)
+    temp = get_answer(index + 1, k)
+
+    if k >= nums[index]:
+        temp = min(temp, get_answer(index + 1, k - nums[index]) + 1)
+
+    dp[index][k] = temp
+    return dp[index][k]
+
+ans = get_answer(0, m)
+
+print(ans if ans != float('inf') else ans)

9b6b007351d78f7194b7b8cb3c0946105e8695eb
Kant Lim <yju0808@naver.com>
2024-10-15 21:01:23 +0900

    Title: 부분 수열의 합이 m, Time: 415ms, Memory: 35MB, Status: Passed - CodeTree

--- 241015/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 241015/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -1,33 +1,34 @@
 MAX_ANS = 101
 
 # 변수 선언 및 입력:
-n, m = tuple(map(int, input().split()))
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # 메모이제이션을 위한 dp 배열 생성
-# dp[i][j] : i번째 원소부터 끝까지 사용하여 합이 j일 때의 최소 수열의 길이
+# dp[i][rem] : i번째 원소부터 끝까지 사용하여 남은 금액 rem을 만들기 위한 최소 수열의 길이
 dp = [[-1] * (m + 1) for _ in range(n + 1)]
 
-def dfs(index, sum):
-    # 합이 정확히 m이 되었을 때
-    if sum == m:
+def dfs(index, rem):
+    # 남은 금액을 정확히 0으로 만들었을 때
+    if rem == 0:
         return 0
-    # 인덱스를 벗어나거나 합이 초과된 경우
-    if index == n or sum > m:
+    # 인덱스를 벗어나거나 남은 금액이 음수가 된 경우
+    if index == n or rem < 0:
         return MAX_ANS
     # 이미 계산한 값이 있으면 반환
-    if dp[index][sum] != -1:
-        return dp[index][sum]
+    if dp[index][rem] != -1:
+        return dp[index][rem]
     
     # 현재 원소를 선택하지 않는 경우
-    res = dfs(index + 1, sum)
+    res = dfs(index + 1, rem)
     # 현재 원소를 선택하는 경우 (각 원소를 최대 한 번씩만 선택)
-    res = min(res, dfs(index + 1, sum + arr[index]) + 1)
+    if rem >= arr[index]:
+        res = min(res, dfs(index + 1, rem - arr[index]) + 1)
     
-    dp[index][sum] = res
+    dp[index][rem] = res
     return res
 
-min_len = dfs(0, 0)
+min_len = dfs(0, m)
 
 # 합 m을 만드는 것이 불가능 할 시, -1을 출력합니다.
 if min_len >= MAX_ANS:

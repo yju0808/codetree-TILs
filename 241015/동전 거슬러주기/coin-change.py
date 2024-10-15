ffbba23c9f2fdbb67bada534f2999fed33a7ab27
Kant Lim <yju0808@naver.com>
2024-10-15 19:41:46 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241015/동전 거슬러주기/coin-change.py
+++ 241015/동전 거슬러주기/coin-change.py
@@ -5,13 +5,14 @@ coins = list(map(int, input().split()))
 dp = [float('inf') for _ in range(m + 1)]
 
 for coin in coins:
-    dp[coin] = 1
+    if coin <= m:
+        dp[coin] = 1
 
 
 def get_answer(k):
 
     if k < 0:
-        return 0
+        return float('inf')
 
     if dp[k] != float('inf'):
         return dp[k]
@@ -27,5 +28,6 @@ def get_answer(k):
     return dp[k]
 
 ans = get_answer(m)
+
 
 print(ans if ans != float('inf') else -1)

4dbf292dae600b64d0fb6ab77eccd356217e5ae5
Kant Lim <yju0808@naver.com>
2024-10-15 19:26:23 +0900

    Title: 알바로 부자 되기, Time: 208ms, Memory: 28MB, Status: Passed - CodeTree

--- 241015/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 241015/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -9,9 +9,12 @@ dp = [0 for _ in range(n)]
 dp[0] = jobs[0][2]
 
 for i in range(1, n):
+
+    dp[i] = jobs[i][2]
+
     for j in range(i):
 
         if jobs[i][0] > jobs[j][1]:
-            dp[i] = max(dp[i], dp[j] + jobs[j][2])
+            dp[i] = max(dp[i], dp[j] + jobs[i][2])
 
 print(max(dp))

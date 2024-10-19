474cb473baf12262bb83f9e1f31ca234944e453a
Kant Lim <yju0808@naver.com>
2024-10-19 11:37:26 +0900

    Title: 배낭 채우기 2, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241019/배낭 채우기 2/knapsack-2.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+goods = [(tuple(map(int, input().split()))) for _ in range(n)]
+
+dp = [0 for _ in range(m + 1)]
+
+for w, v in goods:
+    for i in range(m +1):
+
+        if i >= w:
+            dp[i] = max(dp[i], dp[i - w] + v)
+
+
+print(dp[m])

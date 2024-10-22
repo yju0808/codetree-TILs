635a590a34221b1feee120bb1f6e3418036c1b0d
Kant Lim <yju0808@naver.com>
2024-10-22 21:03:05 +0900

    Title: 고대 보물 지도의 비밀, Time: 358ms, Memory: 40MB, Status: Passed - CodeTree

--- 241022/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 241022/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -6,12 +6,13 @@ nums.insert(0, 0)
 
 dp = [[-float('inf') for _ in range(k + 1)] for _ in range(n + 1)]
 
-dp[0][0] = 0
+for i in range(k + 1):
+    dp[0][i] = 0
 
 for i in range(1, n + 1):
 
     if nums[i] >= 0:
-        for j in range(1, k + 1):
+        for j in range(0, k + 1):
             dp[i][j] = max(nums[i], dp[i - 1][j] + nums[i])
 
     else:
@@ -21,6 +22,7 @@ for i in range(1, n + 1):
 
 
 ans = -float('inf')
+
 
 for i in range(n + 1):
     for j in range(k +1):

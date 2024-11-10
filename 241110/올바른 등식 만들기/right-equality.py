ff5c78007d530335ea41849c22aee9aae574a110
Kant Lim <yju0808@naver.com>
2024-11-10 16:40:05 +0900

    Title: 올바른 등식 만들기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241110/올바른 등식 만들기/right-equality.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.insert(0, 0)
+
+max_num = max(nums)
+
+dp = [[0 for _ in range(41)] for _ in range(len(nums) + 1)]
+dp[0][0] = 1
+dp[0][20] = 1
+
+for i in range(1, len(nums)):
+    for j in range(41):
+
+        current_num = nums[i]
+
+        if j + current_num <= 40:
+            dp[i][j] += dp[i - 1][j + current_num]
+
+        if j - current_num >= 0:
+            dp[i][j] += dp[i - 1][j - current_num]
+
+print(dp[n][m + 20])

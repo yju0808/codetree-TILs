32fa00b800b9c5ee3df0bd9263e28606c37af763
Kant Lim <yju0808@naver.com>
2024-11-11 20:26:48 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 87ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 241111/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+grades = ['G','B','T']
+
+dp = [[[0 for _ in range(4)] for _ in range(4)] for _ in range(n +1)]
+#i 문자열 길이
+# j 총 받은 T의 개수 
+# k 연속으로 B를 받은 개수
+
+
+dp[1][0][0] = 1
+dp[1][1][0] = 1
+dp[1][0][1] = 1
+
+for i in range(2, n + 1):
+    for j in range(4):
+        for k in range(4):
+
+            count = dp[i - 1][j][k]
+
+            dp[i][j][0] += count
+
+            if j < 2:
+                dp[i][j +1][0] += count
+            
+            if k < 2:
+                dp[i][j][k + 1] += count
+
+
+ans = 0
+
+for j in range(4):
+    for k in range(4):
+        ans += dp[n][j][k]
+
+print(ans)

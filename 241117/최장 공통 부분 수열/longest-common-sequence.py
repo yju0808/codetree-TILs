3181f7e7dfe19aa2e481492dd3b893f26964a213
Kant Lim <yju0808@naver.com>
2024-11-17 12:48:46 +0900

    Title: 최장 공통 부분 수열, Time: 90ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241117/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,38 @@
+
+
+a = input()
+b = input()
+
+a = 'X' + a
+b = 'X' + b
+
+dp = [[0 for _ in range(len(b))] for _ in range(len(a))]
+dp[1][1] = 1 if a[0] == b[0] else 0
+
+
+for i in range(1, len(a)):
+    if a[i] == b[1]:
+        dp[i][1] = 1
+    else:
+        dp[i][1] = dp[i - 1][1]
+
+
+for i in range(1, len(b)):
+    if a[1] == b[i]:
+        dp[1][i] = 1
+    else:
+        dp[1][i] = dp[1][i - 1]
+
+for i in range(2, len(a)):
+    for j in range(2, len(b)):
+
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+
+ans = dp[len(a) - 1][len(b) - 1]
+
+print(ans)

d82b373f2bba5f436b382c054b5ac44d27ca7166
Kant Lim <yju0808@naver.com>
2024-11-17 16:00:13 +0900

    Title: 편집거리, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 241117/편집거리/edit-distance.py
+++ 241117/편집거리/edit-distance.py
@@ -8,23 +8,18 @@ b = 'X' + b
 
 dp = [[0 for _ in range(len(b))] for _ in range(len(a))]
 
-dp[1][1] = 0 if a[1] == b[1] else 1
+dp[0][0] = 0
 
 for i in range(1, len(b)):
-    if a[1] == b[i]:
-        dp[1][i] = 1
-    else:
-        dp[1][i] = dp[1][i - 1] + 1
+    dp[0][i] = i
+
 
 for i in range(1, len(a)):
-    if a[i] == b[1]:
-        dp[i][1] = 1
-    else:
-        dp[i][1] = dp[i - 1][1] + 1
+    dp[i][0] = i
 
 
-for i in range(2, len(a)):
-    for j in range(2, len(b)):
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
 
         if a[i] == b[j]:
             dp[i][j] = dp[i - 1][j - 1] 

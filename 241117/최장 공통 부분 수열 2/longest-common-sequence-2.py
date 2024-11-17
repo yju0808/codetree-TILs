2e5c8ea5ce76e14f1c4424aa8d971bd712aecaef
Kant Lim <yju0808@naver.com>
2024-11-17 16:08:45 +0900

    Title: 최장 공통 부분 수열 2, Time: 68ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 241117/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -0,0 +1,52 @@
+
+
+a = input()
+b = input()
+
+a = 'X' + a
+b = 'X' + b
+
+dp = [['' for _ in range(len(b))] for _ in range(len(a))]
+dp[1][1] = a[0] if a[0] == b[0] else ''
+
+
+def get_max_str(arr):
+
+    max_len = len(arr[0])
+    max_i = 0
+
+    for i in range(1, len(arr)):
+        if len(arr[i]) > max_len:
+            max_len = len(arr[i])
+            max_i = i
+
+
+    return arr[i]
+
+
+for i in range(1, len(a)):
+    if a[i] == b[1]:
+        dp[i][1] = a[i]
+    else:
+        dp[i][1] = dp[i - 1][1]
+
+
+for i in range(1, len(b)):
+    if a[1] == b[i]:
+        dp[1][i] = a[i]
+    else:
+        dp[1][i] = dp[1][i - 1]
+
+for i in range(2, len(a)):
+    for j in range(2, len(b)):
+
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] + a[i]
+
+        else:
+            dp[i][j] = get_max_str([dp[i - 1][j], dp[i][j - 1]])
+
+
+ans = dp[len(a) - 1][len(b) - 1]
+
+print(ans)

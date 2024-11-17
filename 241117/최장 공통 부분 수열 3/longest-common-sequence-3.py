974ae255e2efb8a5f1f92f7e5babee554434d9f5
Kant Lim <yju0808@naver.com>
2024-11-17 17:23:12 +0900

    Title: 최장 공통 부분 수열 3, Time: 362ms, Memory: 31MB, Status: Passed - CodeTree

--- 241117/최장 공통 부분 수열 3/longest-common-sequence-3.py
+++ 241117/최장 공통 부분 수열 3/longest-common-sequence-3.py
@@ -12,15 +12,27 @@ dp = [[[] for _ in range(len(b))] for _ in range(len(a))]
 dp[1][1] = [a[1]] if a[1] == b[1] else []
 
 
-def get_max_str(arr1, arr2):
+def get_max_str(arr):
 
-    if len(arr1) > len(arr2):
-        return copy.deepcopy(arr1)
-    elif len(arr1) < len(arr2):
-        return copy.deepcopy(arr2)
+    max_len = len(arr[0])
+    max_i = 0
+
+    for i in range(1, len(arr)):
+        if len(arr[i]) > max_len:
+            max_len = len(arr[i])
+            max_i = i
+
+        elif len(arr[i]) == max_len:
+            for j in range(0, len(arr[i])):
+                if arr[i][j] < arr[max_i][j]:
+                    max_len = len(arr[i])
+                    max_i = i
+                    break
+                elif arr[i][j] > arr[max_i][j]:
+                    break
 
 
-    return copy.deepcopy(arr1) if arr1 < arr2  else copy.deepcopy(arr2)
+    return copy.deepcopy(arr[max_i])
 
 
 for i in range(1, len(a)):
@@ -43,7 +55,7 @@ for i in range(2, len(a)):
             dp[i][j] = copy.deepcopy(dp[i - 1][j - 1]) + [a[i]]
 
         else:
-            dp[i][j] = get_max_str(dp[i - 1][j], dp[i][j - 1])
+            dp[i][j] = get_max_str([dp[i - 1][j], dp[i][j - 1]])
 
 
 ans = dp[len(a) - 1][len(b) - 1]

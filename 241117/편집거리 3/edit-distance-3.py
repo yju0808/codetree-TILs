f9294aaa218439a3ce38419ff0781ddd0d2b281a
Kant Lim <yju0808@naver.com>
2024-11-17 16:02:35 +0900

    Title: 편집거리 3, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241117/편집거리 3/edit-distance-3.py
@@ -0,0 +1,31 @@
+
+
+a = input()
+b = input()
+
+a = 'X' + a
+b = 'X' + b
+
+dp = [[0 for _ in range(len(b))] for _ in range(len(a))]
+
+dp[0][0] = 0
+
+for i in range(1, len(b)):
+    dp[0][i] = i
+
+
+for i in range(1, len(a)):
+    dp[i][0] = i
+
+
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] 
+
+        else:
+            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
+
+
+print(dp[len(a) - 1][len(b) - 1])

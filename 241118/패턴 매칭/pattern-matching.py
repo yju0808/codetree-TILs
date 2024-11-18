ce135b2f462c6bf4d7b02fd7861713ded50565a6
Kant Lim <yju0808@naver.com>
2024-11-18 21:10:37 +0900

    Title: 패턴 매칭, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241118/패턴 매칭/pattern-matching.py
+++ 241118/패턴 매칭/pattern-matching.py
@@ -1,31 +1,34 @@
 
-s = input()
-p = input()
+s = ' ' + input()
+p = ' ' + input()
 
 
 dp = [[False for _ in range(len(p))] for _ in range(len(s))]
-dp[0][0] = True
+dp[0][0] = True  
 
-for i in range(len(s)):
-    for j in range(len(p)):
 
+for j in range(1, len(p)):
+    if p[j] == '*' and j >= 2:
+        dp[0][j] = dp[0][j - 2]
+
+
+for i in range(1, len(s)):
+    for j in range(1, len(p)):
+        
         if p[j] == '*':
+            dp[i][j] = dp[i][j - 2]
+
+            if p[j - 1] == '.' or p[j - 1] == s[i]:
+                dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+        elif p[j] == '.' or p[j] == s[i]:
             dp[i][j] = dp[i - 1][j - 1]
 
-        elif p[j] == '.':
-            dp[i][j] = True
-
-        else:
-            if s[i] == p[j]:
-                dp[i][j] = True
 
 
 ans = 'false'
+if dp[len(s) - 1][len(p) - 1]:
+    ans = 'true'
 
-for i in range(len(p)):
-    if dp[len(s) - 1][i] :
-        ans = 'true'
 
-    
 print(ans)
-

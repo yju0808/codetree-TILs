ab1175d6fd24c5bc5b86e7f0a53d330acfe057ab
Kant Lim <yju0808@naver.com>
2024-11-24 15:11:36 +0900

    Title: 세 수의 합, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241124/세 수의 합/sum-of-three-num.py
+++ 241124/세 수의 합/sum-of-three-num.py
@@ -6,6 +6,13 @@ nums = list(map(int, input().split()))
 d = dict()
 ans = 0
 
+
+for n in nums:
+    if n in d:
+        d[n] += 1
+    else:
+        d[n] = 1
+
 for i in range(len(nums)):
     for j in range(i + 1, len(nums)):
 
@@ -15,12 +22,20 @@ for i in range(len(nums)):
         target = k - (n1 + n2)
 
         if target in d:
-            ans += d[target]
 
-        if n1 + n2 in d:
-            d[n1 + n2] += 1
-        else:
-            d[n1 + n2] = 1
+            count = d[target]
+
+            if target == n1:
+                count -= 1
+            
+            if target == n2:
+                count -= 1
+
+            if count > 0:
+                ans += count
+                d[n1] -= 1
+                d[n2] -= 1
+                
 
 
 print(ans)

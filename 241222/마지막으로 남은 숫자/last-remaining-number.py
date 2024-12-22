3876fcb082057e5dc205ac4182085330e015bfab
Kant Lim <yju0808@naver.com>
2024-12-22 21:21:40 +0900

    Title: 마지막으로 남은 숫자, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241222/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,27 @@
+import heapq as hq
+
+
+n = int(input())
+
+min_heap = []
+
+nums = list(map(int, input().split()))
+
+for n in nums:
+    hq.heappush(min_heap, -n)
+
+    while len(min_heap) >= 2:
+
+        first = -hq.heappop(min_heap)
+            second = -hq.heappop(min_heap)
+
+                new = first - second
+
+                    if new:
+                            hq.heappush(min_heap, - new)
+
+
+                            if min_heap:
+                                print(-min_heap[0])
+                                else:
+                                    print(-1)

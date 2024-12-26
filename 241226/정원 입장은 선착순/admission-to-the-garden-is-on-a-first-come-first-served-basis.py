c51920b4381b7089e02081b3456272d43b89d025
Kant Lim <yju0808@naver.com>
2024-12-26 23:10:42 +0900

    Title: 정원 입장은 선착순, Time: 810ms, Memory: 39MB, Status: Passed - CodeTree

--- 241226/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 241226/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -20,19 +20,19 @@ current_time = min_heap[0][0]
 
 while min_heap:
 
+    current_time = max(current_time, min_heap[0][0])
+
     while min_heap and min_heap[0][0] <= current_time:
         a, i, t = hq.heappop(min_heap)
         hq.heappush(min_heap_another, (i, a, t))
-    
     
     while min_heap_another:
 
         i, a, t = hq.heappop(min_heap_another)
 
         current_time = max(current_time, a)
-        current_time = current_time + t
-
         ans = max(ans, abs(current_time - a))
+        current_time = current_time + t
 
         if min_heap and min_heap[0][0] <= current_time:
             break

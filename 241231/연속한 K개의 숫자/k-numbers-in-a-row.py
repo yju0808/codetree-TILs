d1346c31d509770450264872a4ead8b4c52cad82
Kant Lim <yju0808@naver.com>
2024-12-31 17:58:17 +0900

    Title: 연속한 K개의 숫자, Time: 53ms, Memory: 17MB, Status: Passed - CodeTree

--- 241231/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 241231/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -3,11 +3,12 @@ n, k, b = map(int, input().split())
 
 
 prefix_sum = [0 for _ in range(n)]
-nums = [0 for _ in range(n + 1)]
+nums = [0 for _ in range(n)]
 
 for _ in range(b):
     missing_num = int(input())
-    nums[missing_num - 1] = 1
+    missing_num -= 1
+    nums[missing_num] = 1
 
 prefix_sum[0] = nums[0]
 
@@ -17,7 +18,7 @@ for i in range(1, n):
 
 ans = float('inf')
 
-for i in range(n - k):
+for i in range(n - k + 1):
     temp = prefix_sum[i + k - 1] - prefix_sum[i] + nums[i]
     ans = min(ans, temp)
 

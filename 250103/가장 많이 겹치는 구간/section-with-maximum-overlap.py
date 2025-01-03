ea7de4ec2152b100226905705f28a1753ac3c49c
Kant Lim <yju0808@naver.com>
2025-01-03 18:49:34 +0900

    Title: 가장 많이 겹치는 구간, Time: 91ms, Memory: 20MB, Status: Passed - CodeTree

--- 250103/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 250103/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -4,19 +4,15 @@ line = [0 for _ in range(MAX)]
 
 for _ in range(n):
     x1, x2 = map(int, input().split())
-    line[x1 + 1] += 1
+    line[x1] += 1
     line[x2] -= 1
 
 ans = 0
 current = 0
 for i in range(1, MAX):
     
-    ne = current + line[i]
-
-    if ne > current:
-        ans = ne
-
-    current = ne
+    current += line[i]
+    ans = max(ans, current)
     
 
 print(ans)

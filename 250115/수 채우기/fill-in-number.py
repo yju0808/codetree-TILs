e7326cae81bbba3fd5d24fcb21136b99ff155478
Kant Lim <yju0808@naver.com>
2025-01-15 12:45:49 +0900

    Title: 수 채우기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250115/수 채우기/fill-in-number.py
@@ -0,0 +1,21 @@
+
+
+n = int(input())
+
+count = 0
+current_n = n
+
+for i in range(n):
+
+    if current_n <=  0:
+        break
+
+    if current_n % 5 == 0:
+        count += current_n // 5
+        break
+
+    else:
+        count += 1
+        current_n -= 2
+
+print(count if current_n >= 0 else -1)
